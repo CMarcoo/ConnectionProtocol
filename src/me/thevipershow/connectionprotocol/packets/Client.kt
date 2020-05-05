63a88e8347612295fc0ee8a44d3e7ed30de7aabc
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-05-05 12:43:07 +0200

    Fixed Client session getter

--- src/me/thevipershow/connectionprotocol/packets/Client.kt
+++ src/me/thevipershow/connectionprotocol/packets/Client.kt
@@ -10,5 +10,5 @@ package me.thevipershow.connectionprotocol.packets
 import me.thevipershow.connectionprotocol.packets.packet.PacketProtocol
 
 class Client(val host: String, val port: Int, val protocol: PacketProtocol, factory: SessionFactory) {
-
+    val session: Session = factory.createClientSession(this)
 }
